39ea0975548838ecbfb6533c0b2750dedac36e63
Dave Dane Pacilan <davedanepacilan3p@mail.com>
2017-12-06 07:35:22 +0800

    Extract firebase details and remove non-useful files for security purpose

--- src/components/firebaseInit/index.js
+++ src/components/firebaseInit/index.js
@@ -1,12 +1,6 @@
+import firebaseConfig from '../../../firebase-config.json'
 import firebase from 'firebase'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyCSQTk-Tg9Pif1VbxwdLF--q_xHmlIr2L4',
-  authDomain: 'activity-time-tracker.firebaseapp.com',
-  databaseURL: 'https://activity-time-tracker.firebaseio.com',
-  projectId: 'activity-time-tracker',
-  storageBucket: 'activity-time-tracker.appspot.com',
-  messagingSenderId: '953694498228'
-})
+firebase.initializeApp(firebaseConfig)
 
 export default firebase
